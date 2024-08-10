278ad93289936ce8f3862b8d8fd005be6f5a2cdc
Sanhe Hu <MacHu-GWU@users.noreply.github.com>
2024-08-10 11:04:18 -0400

    Update api.py

--- dbsnaplake/api.py
+++ dbsnaplake/api.py
@@ -4,24 +4,20 @@ from .typehint import T_RECORD
 from .typehint import T_DF_SCHEMA
 from .typehint import T_EXTRACTOR
 from .typehint import T_OPTIONAL_KWARGS
+from .utils import repr_data_size
 from .partition import Partition
 from .partition import extract_partition_data
 from .partition import encode_hive_partition
 from .partition import get_s3dir_partition
 from .partition import get_partitions
-from .s3_loc import S3Location
 from .polars_utils import write_parquet_to_s3
 from .polars_utils import group_by_partition
-from .compaction import calculate_merge_plan
 from .compaction import get_merged_schema
 from .compaction import harmonize_schemas
-from .manifest import DataFile
-from .manifest import T_DATA_FILE
-from .manifest import ManifestFile
-from .manifest import T_MANIFEST_FILE
+from .snapshot_to_staging import DBSnapshotManifestFile
+from .snapshot_to_staging import DBSnapshotFileGroupManifestFile
 from .snapshot_to_staging import DerivedColumn
-from .snapshot_to_staging import SnapshotDataFile
-from .snapshot_to_staging import StagingDataFile
+from .snapshot_to_staging import StagingFileGroupManifestFile
 from .snapshot_to_staging import process_snapshot_data_file
 # from .staging_to_datalake import DatalakeDataFile
 # from .staging_to_datalake import execute_compaction
