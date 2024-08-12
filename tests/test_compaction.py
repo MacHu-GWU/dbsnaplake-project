331e475c3e89f5977f56a617debfe0f85d59f35e
Sanhe Hu <MacHu-GWU@users.noreply.github.com>
2024-08-12 01:54:31 -0400

    compaction

--- tests/test_compaction.py
+++ tests/test_compaction.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 
-import random
-
 import polars as pl
+import pytest
 
 from dbsnaplake.compaction import (
     get_merged_schema,
     harmonize_schemas,
+    merge_dataframes,
 )
 
 
@@ -15,57 +15,41 @@ def test_harmonize_schemas():
         {
             "a": [1, 2, 3],
             "b": ["a", "b", "c"],
+        },
+    )
+    df2 = pl.DataFrame(
+        {
+            "b": ["a", "b", "c"],
             "c": {
                 "id": 1,
                 "name": "Alice",
             },
         },
     )
-    df2 = pl.DataFrame(
+    df3 = pl.DataFrame(
         {
-            "a": [1, 2, 3],
+            "b": [1.1, 2.2, 3.3],
         },
     )
-    dfs = [df1, df2]
+    dfs = [df1, df2, df3]
 
-    schema = get_merged_schema(dfs)
-    df1, df2 = harmonize_schemas([df1, df2], schema)
-    # print(df1, df2)
+    with pytest.raises(TypeError):
+        get_merged_schema(dfs, raise_on_conflict=True)
+    schema = get_merged_schema(dfs, raise_on_conflict=False)
+    assert isinstance(schema["b"], pl.Float64)
 
+    df1_res, df2_res = harmonize_schemas([df1, df2], schema)
+    # print(df1_res, df2_res)  # for debug only
+    assert set(df1_res.schema) == {"a", "b", "c"}
+    assert set(df2_res.schema) == {"a", "b", "c"}
 
-# @moto.mock_aws
-# def test_compact_parquet_files():
-#     bsm = BotoSesManager(region_name="us-east-1")
-#     bucket = "mybucket"
-#     bsm.s3_client.create_bucket(Bucket=bucket)
-#     s3dir_before = S3Path(f"s3://{bucket}/before/")
-#     s3dir_after = S3Path(f"s3://{bucket}/after/")
-#
-#     # create test parquet files
-#     ith_file = 0
-#     for _ in range(1, 1 + 10):
-#         ith_file += 1
-#         n = random.randint(1, 2000)
-#         df = pl.DataFrame(
-#             {"id": list(range(1, 1 + n)), "name": ["a123456789" * 100] * n}
-#         )
-#         s3path = s3dir_before / f"{ith_file}.parquet"
-#         buffer = io.BytesIO()
-#         df.write_parquet(buffer)
-#         s3path.write_bytes(buffer.getvalue())
-#
-#     for _ in range(1, 1 + 10):
-#         ith_file += 1
-#         n = random.randint(1, 2000)
-#         df = pl.DataFrame(
-#             {
-#                 "id": list(range(1, 1 + n)),
-#             }
-#         )
-#         s3path = s3dir_before / f"{ith_file}.parquet"
-#         buffer = io.BytesIO()
-#         df.write_parquet(buffer)
-#         s3path.write_bytes(buffer.getvalue())
+    with pytest.raises(TypeError):
+        merge_dataframes([df1, df2, df3])
+
+    df = merge_dataframes([df1, df2])
+    # print(df)  # for debug only
+    assert set(df.schema) == {"a", "b", "c"}
+    assert df.shape == (6, 3)
 
 
 if __name__ == "__main__":
